d99bcd743c2d6b265c4003c04a6e2c40988ba153
DEDE CAHYADI <111202113415@mhs.dinus.ac.id>
2024-06-12 11:55:47 +0700

    Buat splash screen

---
+++ MD/Gymvision/app/src/main/java/com/dicoding/gymvision/view/activity/SplashScreenActivity.kt
@@ -0,0 +1,61 @@
+package com.dicoding.gymvision.view.activity
+
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.gymvision.R
+import com.dicoding.gymvision.databinding.ActivitySplashScreenBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT: Long = 2000
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        hideSystemBars()
+
+        val progressBar = findViewById<ProgressBar>(R.id.progress_bar)
+
+        Handler().postDelayed({
+            val i = Intent(this@SplashActivity, PreWelcomeActivity::class.java)
+            startActivity(i)
+            finish()
+        }, SPLASH_TIME_OUT)
+
+        playAnimation()
+    }
+
+    private fun hideSystemBars() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.logoGymVision, View.TRANSLATION_Y, -30f, 30f).apply {
+            duration = 1500
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val logoGymVision = ObjectAnimator.ofFloat(binding.logoGymVision, View.ALPHA, 1f).setDuration(100)
+    }
+}
